ba5102ce327532d69cd1a4b1fb682622c8ce448a
JedwardMook <jedwardmook@yahoo.com>
2022-03-18 21:22:01 -0500

    commit tree 59da1cf475e1c4d718529e0afe3dc907419b99b7

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+
+  
+  const [searchListing, setSearchListing] = useState("")
+
+  function handleSearch(newSearch){
+    setSearchListing(newSearch)
+  }
+  
+ 
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header 
+        onSearch={handleSearch}/>
+      <ListingsContainer  
+        search={searchListing}/>
     </div>
   );
 }
--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,44 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, {useEffect, useState} from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({search}) {
+  const [listings, setListing] = useState([])
+
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((r) =>r.json())
+      .then((listings) => {
+        console.log(listings)
+        setListing(listings)
+      })
+  }, []);
+
+  const filterListing= listings.filter(listing => {
+    return listing.description.toLowerCase().includes(search.toLowerCase())
+  })
+
+
+  function deleteListing(id) {
+    const updatedListing = listings.filter(
+      (listing) => listing.id !== id
+    );
+    setListing(updatedListing)
+  }
+
+  const listingsToDisplay = filterListing.map((listing, id) => {
+    return <ListingCard
+      listing={listing}
+      key={id}
+      deleteListing={deleteListing}
+      />
+    });
+
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingsToDisplay}
       </ul>
     </main>
   );
